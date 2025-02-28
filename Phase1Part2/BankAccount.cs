e154beb80384abe0fec1d3718a0241f7c1c96945
Evalenc6 <evanv.prof@gmail.com>
2025-02-28 12:08:54 -0500

    Current Phase1Par2

--- Phase1Part2/BankAccount.cs
+++ Phase1Part2/BankAccount.cs
@@ -1,5 +1,23 @@
+namespace Phase1Part2;
+
 using System;
 
 class BankAccount(){
-    static float balance = 0;
+    priavte static float balance = 100;
+    
+    public BankAccount(){
+        Console.WriteLine("Your bank account has 100 bucks in it");
+    }
+
+    public static AddToBalance(float addAmount){
+        addAmount = (float)Math.Round(addAmount, 2);
+        BankAccount.balance+=addAmount;
+    }
+
+
+    public static removeToBalance(float lossAmount){
+        lossAmount = (float)Math.Round(lossAmount, 2);
+        BankAccount.balance-=lossAmount;
+    }
+
 }
---
+++ Phase1Part2/BankUser.cs
@@ -0,0 +1,14 @@
+namespace Phase1Part2;
+
+using System;
+
+class BankUser{
+    static int userID = 0;
+    int id;
+
+    public BankUser(){
+        id = userID++;
+    }
+
+
+}
